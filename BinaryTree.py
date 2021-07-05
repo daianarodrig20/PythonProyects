06acd6fb91a1f2cfad88f828927e69866d75afb0
daianarodrig20 <daianarodriguez020@gmail.com>
2021-07-05 21:35:42 +0000

    add README

--- BinaryTree.py
+++ BinaryTree.py
@@ -7,8 +7,44 @@ class Node:
         self.right = None # hijo derecho
         self.data = data
 
-    def PrintTree(self):
-        print(self.data)
+    def insert(self, data):
+        # Compara el nuevo valor con el nodo padre
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = Node(data)
+                else:
+                    self.left.insert(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right = Node(data)
+                else:
+                    self.right.insert(data)
+        else:
+            self.data = data
 
-root = Node(22)
+    def PrintTree(self):
+        if self.left:
+            self.left.PrintTree()
+        print(self.data)
+        if self.right:
+            self.right.PrintTree()
+
+#    def PrintTree(self):
+#        print(self.data)
+
+print("Agrego '15' a mi Árbol Binario")
+root = Node(15)
 root.PrintTree()
+print("Agrego 85, 4, 65, 42, 2, 10, 75" )
+root.insert(85)
+root.insert(4)
+root.insert(65)
+root.insert(42)
+root.insert(2)
+root.insert(10)
+root.insert(75)
+
+print("Finalmente el orden de mi Árbol Binario queda de la siguiente forma")
+root.PrintTree()
+
