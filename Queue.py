545790d47e86bc92013f97ad37d85b30c87b116d
daianarodrig20 <daianarodriguez020@gmail.com>
2021-07-05 02:58:36 +0000

    add README

--- Queue.py
+++ Queue.py
@@ -7,26 +7,43 @@ from typing import Any
 class Queue:
     @dataclass
     class _Node:
-        value: Any
+        date: Any
         next: '_Node'
 
-    __slot__ ['begin','end']
+    __slot__ ['_head','_tail']
 
     def __init__(self, iterable = None):
-        self.begin = None
-        self.end = None
+        self.head = None
+        self.tail = None
         if iterable is not None:
-            for value in iterable:
-                self.enqueue(value)
+            for date in iterable:
+                self.enqueue(date)
+
+    def is_empty(self):
+        return self.head == None
+    
+    def _head(self):
+        assert not self.is_empty(), 'La cola se encuentra vacia'
+        return self.head.date
+
+    def _tail(self):
+        assert not self.is_empty(), ' La cola se encuentra vacia'
+        return self.tail.date
 
     def enqueue(self, value):
-
-    def dequeur(self):
-
-    def begin(self):
-        if self.next is not None:
-            return self.dato
+        new_nodo = _Node(value)
+        if self.head == None:
+            self.head = new_nodo
+            self.head = new_nodo
         else:
-            print('La cola esta vacia')
+            self.tail.next = new_nodo
+            self.tail = new_nodo
 
-    def end(self):
+    def dequeue(self):
+        assert not self.is_empty(), 'La cola se encuentra vacia'
+        aux = self.head.date
+        self.head = self.head.next
+        if self.head is None:
+            self.tail is None
+        return aux
+
