e965c396a75385e77de3d589aba31c79c085b1e6
daianarodrig20 <daianarodriguez020@gmail.com>
2021-07-05 23:45:21 -0300

    add README

---
+++ Conjunto.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from dataclasses import dataclass
+from typing import Any
+
+class Conjunto():
+    class _Nodo:
+        dato: Any
+        sig: '_Nodo'
+
+    __slots__ = ['_pri']
+
+    def __init__(self, iterable = None):
+
+    def agregar_elemento():
+
+    def pertenece()
+    
+    def __repr__():
---
+++ Set.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from dataclasses import dataclass
+from typing import Any
+
+class Set():
+    @dataclass
+    class _Node:
+        value: Any
+        next: '_Node'
+
+    __slots__ = ['_pri']
+
+    def __init__():
+
+    def agregar_elemento():
+
+    def __repr__():
